3450007fdfac7036befc55d8efaf21b4661c7eed
NicolasBrulard <nicolas.brulard@free.fr>
2015-09-03 14:27:37 +0200

    Create Constante.java

---
+++ src/Constante.java
@@ -0,0 +1,17 @@
+
+public class Constante{
+	
+	 public static final String ANSI_TEXT_DEFAULT_COLOR = "\u001B[39m";
+     public static final String ANSI_BLACK_BG = "\u001B[40m";
+     public static final String ANSI_RED_BG = "\u001B[41m";
+     public static final String ANSI_GREEN_BG = "\u001B[42m";
+     public static final String ANSI_YELLOW_BG = "\u001B[43m";
+     public static final String ANSI_BLUE_BG = "\u001B[44m";
+     public static final String ANSI_PURPLE_BG = "\u001B[45m";
+     public static final String ANSI_CYAN_BG = "\u001B[46m";
+     public static final String ANSI_WHITE_BG = "\u001B[47m";
+     public static final String ANSI_BG_DEFAULT_COLOR = "\u001B[49m";
+     
+
+	
+}
