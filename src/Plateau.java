8779e96c705a53077c767bc697aac4c0f50e4b5c
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2015-09-03 13:34:28 +0200

    Update Plateau.java

--- src/Plateau.java
+++ src/Plateau.java
@@ -6,24 +6,17 @@ public class Plateau {
 	private int x;
 	private int y;
 	private Forme[][] grille;
-	private int[][] aide;
-	private String[] nom = new String[]{"ROND","TRIANGLE", "CARRE" };
+	private String[] nom = new String[]{"rond","triangle", "carre" };
 	
 	public Plateau(int x, int y) {
 		this.x = x;
 		this.y = y;
 		grille = new Forme[x][y];
-		aide = new int[x][y];
 		Random rd = new Random();
 		for (int i = 0; i < this.x; i++) {
 			for (int j = 0; j < this.y; j++) {
 				int random = rd.nextInt(nom.length);
-				grille[i][j] =  new Forme(nom[random].toLowerCase(), i, j);
-			}
-		}
-		for (int i = 0; i < this.x; i++) {
-			for (int j = 0; j < this.y; j++) {
-				aide[i][j] = i*x+j;
+				grille[i][j] =  new Forme(nom[random], i, j);
 			}
 		}
 	}
@@ -46,43 +39,60 @@ public class Plateau {
         
     }
 	
-	 public void destruction(){
-	        for(int i = 0 ; i < grille.length ; i++){
-	            for(int j = 0 ; j < grille[i].length ; j++){
-	                int chaine = 0;
-	                for(int k = 1 ; k < 5 ; k++){
-	                    try {
-	                        if(grille[i][j].equals(grille[i][j+k]))
-	                            chaine++;
-	                        else{
-	                            if(chaine >= 3){
-	                                for(int l = j ; l < j+chaine ; l++)
-	                                    grille[i][l] = null;
-	                                chaine = 0;
-	                                destruction();
-	                                break;
-	                            }
-	                        }
-	                    } catch (Exception e) {}
-	                }
-	                for(int k = 1 ; k < 5 ; k++){
-	                    try {
-	                        if(grille[i][j].equals(grille[i+k][j]))
-	                            chaine++;
-	                        else{
-	                            if(chaine >= 3){
-	                                for(int l = i ; l < i+chaine ; l++)
-	                                    grille[l][j] = null;
-	                           chaine = 0;
-	                            destruction();
-	                            break;
-	                            }
-	                        }
-	                    } catch (Exception e) {}
-	                }
-	            }
-	        }
-	    }
+	public void destruction(){
+        for(int i = 0 ; i < grille.length ; i++){
+            for(int j = 0 ; j < grille[i].length ; j++){
+                int chaine = 0;
+                for(int k = 0 ; k < 5 ; k++){
+                    try {
+                        if(grille[i][j].equals(grille[i][j+k]))
+                            chaine++;
+                        else{
+                            if(chaine >= 3){
+                                    for(int l = 0 ; l < chaine ; l++){
+                                        grille[i][j+l] = new Forme("tamer", i, j+l);
+                                    };
+                            }
+                                chaine = 0;
+                                break;
+                        }
+                    } catch (Exception e) {
+                            if(chaine >= 3){
+                                    for(int l = 0 ; l < chaine ; l++){
+                                        grille[i][j+l] = new Forme("tamer", i, j+l);
+                                    };
+                            }
+                                chaine = 0;
+                                break;
+                        }
+                }
+                    chaine = 0;
+                for(int k = 0 ; k < 5 ; k++){
+                    try {
+                        if(grille[i][j].equals(grille[i+k][j]))
+                            chaine++;
+                        else{
+                            if(chaine >= 3){
+                                    for(int l = 0 ; l < chaine ; l++){
+                                        grille[i+l][j] = new Forme("tamer", i+l, j);
+                                    };
+                            }
+                                chaine = 0;
+                                break;
+                        }
+                    } catch (Exception e) {
+                            if(chaine >= 3){
+                                    for(int l = 0 ; l < chaine ; l++){
+                                        grille[i+l][j] = new Forme("tamer", i+l, j);
+                                    };
+                            }
+                                chaine = 0;
+                                break;
+                        }
+                }
+            }
+        }
+    }
 
 	public int getX() {
 		return x;
@@ -92,29 +102,24 @@ public class Plateau {
 		return y;
 	}
 	
-	public String toString(){
-		String result = "";
-		for (int i = 0; i < (this.y*2)+4; i++) {
-			result += "++";
-		}
-		result += "\n";
-		for(int i = 0; i < this.x; i++){
-			result += "+";
-			for(int j = 0; j < this.y; j++){
-				result += this.grille[i][j] +"+";
-				
-			}
-			int a = i*x+x;
-			result+=" + "+i*x+" à "+a;
-			
-			result += "+\n";
-		}
-		for (int i = 0; i < (this.y*2)+4; i++) {
-			result += "++";
-		}
-		
-		return result;
-	}
-	
+	 public String toString() {
+		    String ans= quadrillage() + "\n|";
+		    for (int i = 0; i < grille.length; i++) {
+		      for (int j = 0; j < grille[0].length; j++) {
+			    	  ans += grille[i][j]+"|";
+		      }if(i < grille.length - 1) {
+		        ans += "\n"+quadrillage()+"\n|";
+		      }
+		    }
+		    return ans + "\n" + quadrillage();    
+		  }
+		  
+		  private String quadrillage(){
+		    String ans = "+";
+		    for (int i = 0; i < grille[0].length ; i++) {
+		      ans += "─+";
+		    }    
+		    return ans;
+		  }
 
 }
