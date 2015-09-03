8ea4941d2ac9fff7f0427de5f98ffc0617d3a3ec
Scotow <benjamin-lopez@hotmail.fr>
2015-09-03 09:24:49 +0200

    Create Plateau de fruit de mer.java

---
+++ src/Plateau de fruit de mer.java
@@ -0,0 +1,52 @@
+public class Plateau {
+    
+    private final Forme[][] grille;
+    
+    public Plateau(int largeur, int hauteur){
+        this.grille = new Forme[largeur][hauteur];
+    }
+    
+//    public boolean deplacer(Forme forme1, Forme forme2){
+//        if(Math.abs(forme1.getX() - forme2.getX()) > 1 || Math.abs(forme1.getY() - forme2.getY()) > 1)
+//                return false;
+//        Forme[][] tmp = grille.clone();
+//        destruction();
+//        
+//    }
+    
+    public void destruction(){
+        for(int i = 0 ; i < grille.length ; i++){
+            for(int j = 0 ; j < grille[i].length ; j++){
+                int chaine = 0;
+                for(int k = 1 ; k < 5 ; k++){
+                    try {
+                        if(grille[i][j].equals(grille[i][j+k]))
+                            chaine++;
+                        else{
+                            if(chaine >= 3)
+                                for(int l = j ; l < j+chaine ; l++)
+                                    grille[i][l] = null;
+                            chaine = 0;
+                            break;
+                        }
+                    } catch (Exception e) {}
+                }
+                for(int k = 1 ; k < 5 ; k++){
+                    try {
+                        if(grille[i][j].equals(grille[i+k][j]))
+                            chaine++;
+                        else{
+                            if(chaine >= 3)
+                                for(int l = i ; l < i+chaine ; l++)
+                                    grille[l][j] = null;
+                            chaine = 0;
+                            break;
+                        }
+                    } catch (Exception e) {}
+                }
+            }
+        }
+    }
+    
+    
+}
