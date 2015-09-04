8ae7d29a2b9a8d7026ff25257cf2abb2ee6d35b3
Scotow <benjamin-lopez@hotmail.fr>
2015-09-04 13:39:53 +0200

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,11 +7,10 @@
 package GUI;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.JPanel;
@@ -27,47 +26,90 @@ public class GamePanel extends JPanel implements MouseListener{
     
     private Plateau plateau;
     private Forme[] clics;
+    private final Timer refreshTimer;
     
     public GamePanel(){
         setFocusable(true);
         addMouseListener(this);
         setBackground(Color.BLACK);
         this.plateau = new Plateau(10,10);
+        do {
+            plateau.glisser();
+        } while (plateau.destruction());
         this.clics = new Forme[2];
+        
+        refreshTimer = new Timer(2000, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                graphDestruction();
+            }
+        });
+
+        refreshTimer.start();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
-        for(Forme[] i : plateau.getGrille())
-            for(Forme j : i){
-                g.setColor(j.getGraphiqueColor());
-                //g.fillOval(j.getX()*getWidth()/plateau.getX(), j.getY()*getHeight()/plateau.getY(), getWidth()/plateau.getX(), getHeight()/plateau.getY());
-                switch(j.getNom()){
+        super.paintComponent(g);
+        for(int i = 0 ; i < plateau.getGrille().length ; i++){
+            for(int j = 0 ; j < plateau.getGrille()[i].length ; j++){
+                g.setColor(plateau.getGrille()[i][j].getGraphiqueColor());
+                if(plateau.getGrille()[i][j] == clics[0])
+                    g.setColor(Color.YELLOW);
+                switch(plateau.getGrille()[i][j].getNom()){
                     case "rond":
-                        g.fillOval(j.getX()*getWidth()/plateau.getX(), j.getY()*getHeight()/plateau.getY(), getWidth()/plateau.getX(), getHeight()/plateau.getY());
+                        g.fillOval(j*getWidth()/plateau.getX(), i*getHeight()/plateau.getY(), getWidth()/plateau.getX(), getHeight()/plateau.getY());
                         break;
                     case "carre":
-                        g.fillRect(j.getX()*getWidth()/plateau.getX(), j.getY()*getHeight()/plateau.getY(), getWidth()/plateau.getX(), getHeight()/plateau.getY());
+                        g.fillRect(j*getWidth()/plateau.getX(), i*getHeight()/plateau.getY(), getWidth()/plateau.getX(), getHeight()/plateau.getY());
                         break;
-                    default:
-                        g.fillPolygon(new int[]{j.getX()*getWidth()/plateau.getX()+70, j.getX()*getWidth()/plateau.getX()+35, j.getX()*getWidth()/plateau.getX()}, new int[]{j.getY()*getHeight()/plateau.getY()+55, j.getY()*getHeight()/plateau.getY(), j.getY()*getHeight()/plateau.getY()+55}, 3);
+                    case "triangle":
+                        g.fillPolygon(new int[]{j*getWidth()/plateau.getX()+70, j*getWidth()/plateau.getX()+35, j*getWidth()/plateau.getX()}, new int[]{i*getHeight()/plateau.getY()+55, i*getHeight()/plateau.getY(), i*getHeight()/plateau.getY()+55}, 3);
                         break;
                     case "losange":
-                        g.fillPolygon(new int[]{j.getX()*getWidth()/plateau.getX()+5, j.getX()*getWidth()/plateau.getX()+35, j.getX()*getWidth()/plateau.getX()+65, j.getX()*getWidth()/plateau.getX()+35}, new int[]{j.getY()*getHeight()/plateau.getY()+27, j.getY()*getHeight()/plateau.getY(), j.getY()*getHeight()/plateau.getY()+27, j.getY()*getHeight()/plateau.getY()+55}, 4);
+                        g.fillPolygon(new int[]{j*getWidth()/plateau.getX()+5, j*getWidth()/plateau.getX()+35, j*getWidth()/plateau.getX()+65, j*getWidth()/plateau.getX()+35}, new int[]{i*getHeight()/plateau.getY()+27, i*getHeight()/plateau.getY(), i*getHeight()/plateau.getY()+27, i*getHeight()/plateau.getY()+55}, 4);
                         break;
+                    case "Vide":
+                        g.setColor(Color.WHITE);
+                        g.setFont(new Font(getFont().getName(), getFont().getStyle(), 74));
+                        g.drawString("★", j*getWidth()/plateau.getX(), (i+1)*getHeight()/plateau.getY());                   
+                    break;
+                        
                 }
             }
+        }
     }
     
     public void choixClic(int x, int y){
         if(clics[0] == null){
-            clics[0] = plateau.getGrille()[x/plateau.getX()/(getWidth()/100)][y/plateau.getY()/(getHeight()/100)];
+            clics[0] = plateau.getGrille()[y/(getHeight()/plateau.getY())][x/(getWidth()/plateau.getX())];
+            //plateau.destruction();
+            repaint();
         }else  {
-            clics[1] = plateau.getGrille()[x/plateau.getX()/(getWidth()/100)][y/plateau.getY()/(getHeight()/100)];
+            clics[1] = plateau.getGrille()[y/(getHeight()/plateau.getY())][x/(getWidth()/plateau.getX())];
             plateau.deplacer(clics[0], clics[1]);
+            graphDestruction();
             clics[0] = null;
             clics[1] = null;
+            
+        }
+    }
+    
+    public void graphDestruction(){
+        if(plateau.destruction()){
+                repaint();
+                Timer waitDestruction = new Timer(1000, new ActionListener() {
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        plateau.glisser();
+                        repaint();
+                        ((Timer)e.getSource()).stop();
+                    }
+                });
+                waitDestruction.start();
+        
         }
     }
     
@@ -85,8 +127,9 @@ public class GamePanel extends JPanel implements MouseListener{
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        System.out.println(e.getX()/plateau.getX()/(getWidth()/100) + "\t" + e.getY()/plateau.getY()/(getHeight()/100));
-        //choixClic(e.getX(), e.getY());
+        //System.out.println(e.getX()/(getWidth()/plateau.getX()) + "\t" + e.getY()/(getHeight()/plateau.getY()));
+        choixClic(e.getX(), e.getY());
+        //System.out.println(e.getX() + "\t" + e.getY());
         //System.out.println(e.getX()/getWidth()/plateau.getX());
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
